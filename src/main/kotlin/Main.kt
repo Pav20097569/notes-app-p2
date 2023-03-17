4160d42dff2ce872fa894559da380d25a955921f
Pav20097569 <20097569@mail.wit.ie>
2023-03-17 19:49:05 +0000

    Added more .gitignore code and changed tracked files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -33,7 +33,7 @@ fun runMenu() {
             20 -> save()
             21 -> load()
             0 -> exitApp()
-            else -> System.out.println("Invalid option entered: ${option}")
+            else -> System.out.println("invalid option entered: ${option}")
         }
     } while (true)
 }
